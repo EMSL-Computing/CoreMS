1c137b14dde769c9291ba7dddf3e25719117f15e
Corilo, Yuri <corilo@pnnl.gov>
2019-10-28 17:40:19 -0700

    up version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="coreMS",
-    version="3.0.a0",
+    version="3.1.a0",
     description="Object Oriented Mass Spectrometry ToolBox for Small Molecules Analysis",
     long_description=README,
     long_description_content_type="text/markdown",
