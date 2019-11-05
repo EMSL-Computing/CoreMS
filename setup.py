a82bed270874cf90eb72209df2f6a124aa649241
Corilo, Yuri <corilo@pnnl.gov>
2019-11-05 11:07:46 -0800

    change masslist hearder translation mecanism

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="coreMS",
-    version="5.2.0.alpha",
+    version="5.2.1.alpha",
     description="Object Oriented Mass Spectrometry ToolBox for Small Molecules Analysis",
     long_description=README,
     long_description_content_type="text/markdown",
