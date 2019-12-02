ba7fbb3fb72c26554f22314db6cbb76808586511
Corilo, Yuri <corilo@pnnl.gov>
2019-12-02 10:31:28 -0800

    fix bug issue #26 exportms

--- corems/mass_spectrum/output/export.py
+++ corems/mass_spectrum/output/export.py
@@ -268,8 +268,8 @@ class MassSpecExport(Thread):
         atoms_in_order = Atoms.atoms_order
         all_used_atoms = set()
         for ms_peak in mass_spectrum:
-            for m_formula in ms_peak:
-                if ms_peak:
+            if ms_peak:
+                for m_formula in ms_peak:
                     for atom in m_formula.atoms:
                         all_used_atoms.add(atom)
 
