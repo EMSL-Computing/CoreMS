fa7227ead68dd90031f7b085101ca4027788adf2
Corilo, Yuri <corilo@pnnl.gov>
2020-03-25 10:55:52 -0700

    pin pyside to 5.14.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text(encoding='utf-8')
 # This call to setup() does all the work
 setup(
     name="CoreMS",
-    version="9.11.0.beta",
+    version="9.11.1.beta",
     description="Object Oriented Mass Spectrometry ToolBox for Small Molecules Analysis",
     long_description=README,
     long_description_content_type="text/markdown",
