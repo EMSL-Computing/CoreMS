84feace6d34a2013f2d1fc07ff1e12bd26a29366
Corilo, Yuri <corilo@pnnl.gov>
2020-03-31 14:04:11 -0700

    metabolite_detector_score fix

--- corems/molecular_id/search/compoundSearch.py
+++ corems/molecular_id/search/compoundSearch.py
@@ -36,7 +36,7 @@ class LowResMassSpectralMatch(Thread):
 
         #print(ref_obj.get('ri'), gc_peak.ri, CompoundSearchSettings.ri_window)
 
-        ri_score = exp(-((gc_peak.ri - ref_obj.get('ri'))**2 ) / (2 * CompoundSearchSettings.ri_window))
+        ri_score = exp(-((gc_peak.ri - ref_obj.get('ri'))**2 ) / (2 * CompoundSearchSettings.ri_window**2))
 
         similarity_score = ((spectral_similarity_score**2) * (ri_score))**(1/3)
 
@@ -166,7 +166,7 @@ class LowResMassSpectralMatch(Thread):
 
 
     def euclidean_distance(self, mass_spec, ref_obj):
-        
+
         def euclidean_distance_manual(qlist,rlist):
 
             T1=sum(subtract(qlist,rlist)**2)
