bdaabaa390bb1792135030cc053afadeb960daba
Corilo, Yuri <corilo@pnnl.gov>
2020-05-26 19:08:09 -0700

    eval bump version patch up

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('requirements.txt') as f:
 # This call to setup() does all the work
 setup(
     name="CoreMS",
-    version="14.1.4.beta",
+    version="14.1.5.beta",
     description="Mass Spectrometry Framework for Small Molecules Analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
