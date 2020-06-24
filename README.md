01951d0a3ba426df7cf5f5bd86489aa224fb228d
Corilo, Yuri <corilo@pnnl.gov>
2020-06-24 10:14:38 -0700

    adding mf string output

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Data handling and software development for modern mass spectrometry (MS) is an i
 
 ## Current Version
 
-### `15.1.1.beta`
+### `15.3.0.beta`
 
 ### Data input formats
 
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('requirements.txt') as f:
 # This call to setup() does all the work
 setup(
     name="CoreMS",
-    version="15.1.1.beta",
+    version="15.3.0.beta",
     description="Mass Spectrometry Framework for Small Molecules Analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
