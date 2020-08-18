c8b81cb5156e2f67597f5c3bc89c967fffc23b84
Corilo, Yuri <corilo@pnnl.gov>
2020-08-18 11:30:41 -0700

    deconvolution window and clutering

--- corems/mass_spectra/calc/GC_Deconvolution.py
+++ corems/mass_spectra/calc/GC_Deconvolution.py
@@ -16,6 +16,7 @@ class MassDeconvolution:
         maximum_tic = max(self._processed_tic)
 
         eic_dict = self.ion_extracted_chroma(self._ms)
+        
         peaks_entity_data = self.find_peaks_entity(eic_dict)
         
         # select model peaks, create Mass Spectrum objs, GCPeak objs, store results in GC_Class gcpeaks obj 
@@ -65,8 +66,49 @@ class MassDeconvolution:
         
         return eic_dict           
 
-    def find_peaks_entity(self, eic_dict):
+    def hc(self, X, Y, max_rt_distance=0.025):
         
+        from scipy.cluster.hierarchy import dendrogram, linkage
+        from scipy.cluster.hierarchy import fcluster
+        from matplotlib import pyplot as plt
+
+        Z = linkage(np.reshape(X, (len(X), 1)), method  = "ward")
+        #Z = linkage(X, method  = "ward")
+        
+        max_d = max_rt_distance
+        distance_clusters = fcluster(Z, max_d, criterion='distance')
+        # print("distance")
+        # print(distance_clusters)
+
+        #inconsistency_cluster = fcluster(Z, 2, depth=2)
+        #max_cluster = fcluster(Z, 2, criterion='maxclust')
+
+        grouped_rt = {}
+        for index_obj, group in enumerate(distance_clusters):
+           
+            if not group in grouped_rt.keys():
+                grouped_rt[group] = [X[index_obj]]
+            else:
+                grouped_rt[group].append(X[index_obj])
+        
+        return grouped_rt      
+
+        #plt.figure(figsize=(10, 8))
+        # plt.scatter(X, Y, c=distance_clusters, cmap='prism')  # plot points with cluster dependent colors
+        # plt.show()
+        #labelList = range(int(min(X)), int(max(X)))
+
+        # plt.figure(figsize=(10, 7))
+        # dendrogram(Z,
+        #            orientation='top',
+        #            distance_sort='descending',
+        #            show_leaf_counts=True)
+        # plt.show()
+        # print(Z)
+
+    
+    def find_peaks_entity(self, eic_dict):
+        ''' combine eic with mathing rt apexes''' 
         max_prominence = self.chromatogram_settings.peak_max_prominence_percent
         
         max_height = self.chromatogram_settings.peak_height_max_percent
@@ -83,24 +125,24 @@ class MassDeconvolution:
             
             eic = eic_scan_index_rt[0]
             rt_list = eic_scan_index_rt[1]
-                                    
+
             if len(eic) > 10:
 
                 smooth_eic = self.smooth_tic(eic)
 
-                include_indexes = sp.find_minima_derivative(rt_list, smooth_eic,  max_height, max_prominence, max_eic, correct_baseline=False)
+                include_indexes = sp.find_minima_derivative(rt_list, smooth_eic,  max_height, max_prominence, max_eic, signal_threshold=0.01,  correct_baseline=False)
 
                 #include_indexes = list(self.centroid_detector(smooth_eic, rt_list))
                 
                 for initial_scan, apex_scan, final_scan in include_indexes:
 
-                        rt_corrected_therm = 0 #self.quadratic_interpolation(rt_list, smooth_eic, apex_scan)
+                        rt_corrected_therm =  0 #self.quadratic_interpolation(rt_list, smooth_eic, apex_scan)
                         
                         ref_apex_rt = round(rt_list[apex_scan] + rt_corrected_therm,4)
+                        
                         apex_rt = rt_list[apex_scan]
-                        
                         apex_abundance = smooth_eic[apex_scan]
-                        
+
                         #maximum_tic = apex_abundance if apex_abundance > maximum_tic else maximum_tic
                         
                         for scan_index in range(initial_scan, final_scan):
@@ -132,7 +174,8 @@ class MassDeconvolution:
                                     existing_data['abundance'].append(peak_abundance)
                                     existing_data['scan_number'].append(scan_index)    
         
-        return peaks_entity_data                            
+        
+        return peaks_entity_data
         
     def deconvolution(self, peaks_entity_data, maximum_tic):
         
@@ -140,6 +183,195 @@ class MassDeconvolution:
         tic_list = []
         rt_list = []
 
+        domain = rt_list
+        signal = self._processed_tic
+        max_height = 1 # baseline detector 
+        max_prominence = 0.1 # baseline detector 
+        max_signal = maximum_tic
+        signal_threshold = 0.2
+        correct_baseline = False
+        max_rt_distance = 0.035
+
+        include_indexes = sp.find_minima_derivative(domain, signal,  max_height, max_prominence, max_signal, signal_threshold=signal_threshold, correct_baseline=correct_baseline)
+        
+        ''' deconvolution window is defined by the TIC peak region'''
+        all_apexes_rt = np.array(list(peaks_entity_data.keys()))
+
+        for indexes_tuple in include_indexes:
+            
+            start_rt = self.retention_time[indexes_tuple[0]]
+            apex_rt = self.retention_time[indexes_tuple[1]]
+            final_rt = self.retention_time[indexes_tuple[2]]
+
+            #plt.plot(self.retention_time[indexes_tuple[1]], signal[indexes_tuple[1]], marker= '^', c='red',  linewidth=0)
+            #plt.plot(self.retention_time[indexes_tuple[0]], signal[indexes_tuple[0]], marker= '^', c='blue',  linewidth=0)
+            #plt.plot(self.retention_time[indexes_tuple[2]], signal[indexes_tuple[2]], marker= '^', c='blue',  linewidth=0)
+            
+            ''' find all features within TIC peak window'''
+            peak_features_indexes = np.where((all_apexes_rt >= start_rt) & (all_apexes_rt <= final_rt))[0]
+            peak_features_rts = all_apexes_rt[peak_features_indexes]
+
+            filtered_features_rt = []
+            filtered_features_abundance = []
+            
+            for each_apex_rt in peak_features_rts:
+                
+                apex_data = peaks_entity_data.get(each_apex_rt).get(each_apex_rt)
+
+                peak_features_tic = sum(peaks_entity_data.get(each_apex_rt).get(each_apex_rt).get('abundance'))
+            
+                norm_smooth_tic = (peak_features_tic/ maximum_tic)*100
+
+                ''' TODO: 
+                    Improve Peak Filtering
+
+                    Calculate peaks sharpness here and filter it out (Amax - An /n)?
+                    Peak Fit and Calculate Peak Gaussian Similarity?
+                    Currentely using flat % tic relative abundance threshold and min 3 m/z per mass spectrum
+                '''
+                if norm_smooth_tic > signal_threshold and len(apex_data['mz']) > 3:
+                       
+                       #print(len(apex_data['mz']))
+                       filtered_features_rt.append(each_apex_rt)
+                       filtered_features_abundance.append(peak_features_tic)
+            
+            if len(filtered_features_rt) > 1: 
+                ''' more than one peak feature identified inside a TIC peak  '''
+                # plt.plot(self.retention_time[indexes_tuple[0]:indexes_tuple[2]], signal[indexes_tuple[0]:indexes_tuple[2]], c='black')
+
+                print(filtered_features_rt)
+                grouped_rt = self.hc(filtered_features_rt, filtered_features_abundance, max_rt_distance=max_rt_distance)
+                print(grouped_rt)
+                
+                for group, apex_rt_list in grouped_rt.items():
+                    ''' each group is a peak feature defined by the hc algorithm
+                        summing the apex data only, for now
+                    '''
+                    group_datadict = {}
+                    
+                    for each_group_apex_rt in apex_rt_list:
+                        
+                        datadict = peaks_entity_data.get(each_group_apex_rt)
+
+                        for rt, each_datadict in datadict.items():
+                            
+                            if rt != "ref_apex_rt":
+                                
+                                if rt in group_datadict.keys():
+                                    
+                                    mz_list = each_datadict.get("mz")
+                                    abundance_list = each_datadict.get("abundance")
+                                    
+                                    each_mz_abun = dict(zip(mz_list, abundance_list)) 
+
+                                    for index_mz, mz in enumerate(group_datadict[rt].get("mz")):
+                                        if mz in each_mz_abun.keys():
+                                            
+                                            each_mz_abun[mz] = each_mz_abun[mz] + group_datadict[rt].get("abundance")[index_mz]
+                                        
+                                        else:    
+                                            
+                                            each_mz_abun[mz] = group_datadict[rt].get("abundance")[index_mz]
+                                    
+                                    group_datadict[rt] = { 'mz': list(each_mz_abun.keys()) , 
+                                                        'abundance': list(each_mz_abun.values()), 
+                                                        'scan_number': each_datadict.get('scan_number') }
+                                                        
+
+                                else:
+                                    
+                                    group_datadict[rt] = each_datadict
+                                    group_datadict['ref_apex_rt'] = datadict.get('ref_apex_rt')
+                            
+                    peak_rt = []
+                    peak_tic = []
+
+                    for rt, each_datadict in group_datadict.items():
+                        #print(each_datadict)
+                        if rt != "ref_apex_rt":
+                            peak_rt.append(rt)
+                            peak_tic.append(sum(each_datadict["abundance"]))
+                    
+                    peak_rt, peak_tic = zip(*sorted(zip(peak_rt, peak_tic)))
+
+                    plt.plot(peak_rt, peak_tic)
+
+                # peak_rt.append(rt)
+                # peak_tic.append(sum(each_datadict["abundance"]))
+
+            elif len(filtered_features_rt) == 1:
+                ''' only one peak feature inside deconvolution window '''
+                
+                each_apex_rt = filtered_features_rt[0]
+
+                datadict = peaks_entity_data.get(each_apex_rt)
+
+                apex_data = datadict.get(each_apex_rt)
+
+                tic = sum(apex_data.get('abundance'))
+
+                scan_index = apex_data['scan_number'][0]
+
+                ref_apex_rt = datadict["ref_apex_rt"]
+                
+                mz_list, abundance_list = zip(*sorted(zip(apex_data['mz'], apex_data['abundance'])))
+
+                data_dict = {Labels.mz: mz_list, Labels.abundance: abundance_list}
+                
+                d_params = default_parameters(self._ms[scan_index]._filename)
+                
+                d_params["rt"] = apex_rt
+
+                d_params["scan_number"] = scan_index
+
+                d_params['label'] = Labels.gcms_centroid
+
+                d_params["polarity"] = self._ms[scan_index].polarity
+
+                d_params['analyzer'] = self._ms[scan_index].analyzer
+
+                d_params['instrument_label'] = self._ms[scan_index].instrument_label
+                
+                d_params["filename_path"] = self._ms[scan_index].instrument_label
+
+                ''' Only stores the apex data 
+                    TODO Store all ms on the GCPeak obj
+                    It Might be necessary to to create a new obj or change workflow with out deconvolution
+                    Indexes on original objs are related to the TIC 
+                '''
+                
+                ms = MassSpecCentroidLowRes(data_dict, d_params )
+                
+                '''TODO fake indexes, area calculation will fail 
+                    workaround is to calculate the area here and store the result after the gcpeak obj is created
+                '''
+                gc_peak =  GCPeak(ms, (i-1,i, i+1))
+                i += 1
+
+                self.gcpeaks.append(gc_peak)
+
+                tic_list.append(tic)
+                rt_list.append(ref_apex_rt)
+
+                peak_rt = []
+                peak_tic = []
+                
+                for rt, each_datadict in datadict.items():
+                    
+                    if rt != "ref_apex_rt":
+                        peak_rt.append(rt)
+                        peak_tic.append(sum(each_datadict["abundance"]))
+                    
+                peak_rt, peak_tic = zip(*sorted(zip(peak_rt, peak_tic)))
+
+                plt.plot(peak_rt, peak_tic)
+                
+
+            else:
+                
+                #print('no data after filter')
+                pass
+        '''        
         for apex_rt, datadict in sorted(peaks_entity_data.items()):
             
             if apex_rt in datadict.keys():
@@ -219,6 +451,9 @@ class MassDeconvolution:
                     #plt.show()
                     #plt.close()
         #self.rt_clustering(rt_list, tic_list)
+        
+        '''
+
         plt.plot(self.retention_time, self._processed_tic, c='black')
         plt.plot(rt_list, tic_list, c='black', marker= '^', linewidth=0)
         plt.show()
