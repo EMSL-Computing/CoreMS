de26ef2e9e581529e3a18c2854e5fb0b84e21078
Thompson, Allison M <allison.thompson@pnnl.gov>
2020-08-26 14:22:00 -0700

    adding catch for apex_index equal to start/end

--- corems/mass_spectra/calc/SignalProcessing.py
+++ corems/mass_spectra/calc/SignalProcessing.py
@@ -74,8 +74,17 @@ def peak_picking_first_derivative(domain, signal, max_height, max_prominence, ma
     
     # take apex_index and move left to find start
     for index in apex_indexes:
-        index_start = index - 1
-        index_end = index + 1
+        # catch for starting position
+        if index == 0:
+            index_start = index
+        else:
+            index_start = index - 1
+
+        # catch for ending position
+        if index == (len(dy) - 1):
+            index_end = index
+        else:
+            index_end = index + 1
 
         #while dy[index_start-1] > 0 and index_start != 0:
         while dy[index_start-1] > (max(dy)*0.0005) and index_start > 0:
@@ -83,14 +92,12 @@ def peak_picking_first_derivative(domain, signal, max_height, max_prominence, ma
         start_peak.append(index_start)
         
         #while dy[index_end] < 0 and index_end != (len(dy) - 1):
-        while dy[index_end] < (min(dy)*0.0005) and index_end < (len(dy) - 1):
+        while dy[index_end] < (min(dy)*0.0005) and index_end != (len(dy) - 1):
             index_end = index_end + 1
         end_peak.append(index_end)
 
     start_peak = array(start_peak)
     end_peak = array(end_peak)
-
-
 
     # left_index = []
     # right_index = []
