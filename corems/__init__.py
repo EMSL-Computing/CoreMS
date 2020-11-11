a3c525d74143d5e975d0130592f7257040a681fe
Corilo, Yuri <corilo@pnnl.gov>
2020-11-11 12:06:39 -0800

    version up

--- corems/__init__.py
+++ corems/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'Yuri E. Corilo'
-__version__ = '22.0.7.beta'
+__version__ = '22.0.8.beta'
 __doc__ = '''
 CoreMS - a powerful framework for mass spectrometry data processing and analysis of small molecules
 =====================================================================
