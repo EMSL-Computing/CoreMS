93b3da35a7fef01fb25776130ea1e797e845cd48
Corilo, Yuri <corilo@pnnl.gov>
2021-01-28 22:50:59 +0000

    fillnan from hdf5 file

--- corems/__init__.py
+++ corems/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'Yuri E. Corilo'
-__version__ = '22.1.13.beta'
+__version__ = '22.1.14.beta'
 __doc__ = '''
 CoreMS - a powerful framework for mass spectrometry data processing and analysis of small molecules
 =====================================================================
