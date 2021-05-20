64a64210bf72c8cbcee8e66893c9bb7fc96ede2c
Corilo, Yuri <corilo@pnnl.gov>
2021-05-20 15:48:52 -0700

    output min score

--- README.md
+++ README.md
@@ -39,7 +39,7 @@ Data handling and software development for modern mass spectrometry (MS) is an i
 
 ## Current Version
 
-### `2.0.1.beta`
+### `2.1.0.beta`
 
 ## Main Developers/Contact 
 - [Yuri. E. Corilo](mailto:corilo@pnnl.gov)  
--- corems/encapsulation/factory/processingSetting.py
+++ corems/encapsulation/factory/processingSetting.py
@@ -210,6 +210,8 @@ class CompoundSearchSettings:
 
     output_score_method: str = 'All'
 
+    
+
     def __post_init__(self):
         # enforce datatype
         self.url_database = os.getenv('SPECTRAL_GCMS_DATABASE_URL', 'sqlite:///db/pnnl_lowres_gcms_compounds.sqlite')
@@ -360,6 +362,8 @@ class MolecularFormulaSearchSettings:
     score_method: str = 'prob_score'
 
     output_score_method: str = 'All Candidates'
+
+    output_min_score: float = 0.1
 
     # depending on the polarity mode it looks for [M].+ , [M].-
     # query and automatically compile add entry if it doesn't exist
