e555c3dc605e335fa562097cf87f24f6093a0d34
Yuri <corilo@pnnl.gov>
2021-11-17 15:49:08 -0800

    start icp ms signal processing

--- examples/scripts/LC-ICPMS_metal_peaks.py
+++ examples/scripts/LC-ICPMS_metal_peaks.py
@@ -47,8 +47,21 @@ def find_metal_peaks(metal, icp_data, sn, min_peak_datapoints):
 	for peak_index in peak_indices:
 		rt_aps.append(rt[peak_index[1]])
 
-	rt_dict = dict.fromkeys(rt_aps,metal)
+	rt_dict = {metal:rt_aps}
 	return rt_dict
+
+def smooth_signal(signal):
+            
+        implemented_smooth_method = ('savgol', 'hanning', 'blackman', 'bartlett', 'flat', 'boxcar')
+        
+        pol_order = 2
+
+        window_len = 301
+
+        smooth_method = 'savgol'
+
+
+        return sp.smooth_signal(signal, window_len, smooth_method, pol_order, implemented_smooth_method)
 
 if __name__ == '__main__':
 	
@@ -56,19 +69,25 @@ if __name__ == '__main__':
 	icpdata = pd.read_csv(icpfile)
 
 	for columns_label in icpdata.columns[1:]:
+		
 		if columns_label[0:4] != 'Time':
-			
 			
 			metal = columns_label
 
-			icp_signal = icpdata[metal]  
+			icp_signal = smooth_signal(icpdata[metal])
+			
 			min_peak_datapoints = 20
 
 			rts = find_metal_peaks(metal, icpdata, 1,  min_peak_datapoints)
+			
 			print(rts)
 
 			fig, host = plt.subplots()
-			host.plot(icpdata['Time '+ metal ], icpdata[metal])
+			
+			host.plot(icpdata['Time '+ metal ], icp_signal)
+			
 			host.set_xlabel('Time (s)')
+			
 			host.set_ylabel(metal +' intensity (counts)')
+			
 			plt.show()
