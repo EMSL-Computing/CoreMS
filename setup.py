9a074750da82cceec7a86f5095324b51392beb35
Yuri <corilo@pnnl.gov>
2023-01-05 14:53:46 -0800

    update python requirements

--- setup.py
+++ setup.py
@@ -23,8 +23,10 @@ setup(
     author_email="corilo@pnnl.gov",
     classifiers=[
         "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Development Status :: 4 - Beta"
     ],
 
