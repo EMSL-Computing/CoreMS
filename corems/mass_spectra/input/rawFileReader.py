a08a231bb2a6b4fb75fca75fd2b7ce86dde240c5
Katherine Heal <katherine.heal@pnnl.gov>
2024-02-08 15:51:08 -0800

    Clean up spectra parsers

--- corems/mass_spectra/input/rawFileReader.py
+++ corems/mass_spectra/input/rawFileReader.py
@@ -58,8 +58,7 @@ from System.Collections.Generic import List
 
 
 class ThermoBaseClass:
-    """
-    Class for reading Thermo Raw files and extracting information from them.
+    """Class for parsing Thermo Raw files and extracting information from them.
 
     Parameters:
     -----------
@@ -868,161 +867,7 @@ class ThermoBaseClass:
         mass_spec = MassSpecProfile(data_dict, d_params, auto_process=auto_process)
 
         return mass_spec
-
-class ImportMassSpectraThermoMSFileReader(ThermoBaseClass, LC_Calculations):
-
-    """Collection of methdos to import Summed/Averaged mass spectrum from Thermo's raw file
-    Currently only for profile mode data
-    Returns MassSpecProfile object
-    """
-
-    def get_icr_transient_times(self):
-        """
-        Return a list for transient time targets for all scans, or selected scans range
-        Resolving Power and Transient time targets based on 7T FT-ICR MS system
-        """
-
-        res_trans_time = {
-            "50": 0.384,
-            "100000": 0.768,
-            "200000": 1.536,
-            "400000": 3.072,
-            "750000": 6.144,
-            "1000000": 12.288,
-        }
-
-        firstScanNumber = self.start_scan
-
-        lastScanNumber = self.end_scan
-
-        transient_time_list = []
-
-        for scan in range(firstScanNumber, lastScanNumber):
-            scan_header = self.get_scan_header(scan)
-
-            rp_target = scan_header["FT Resolution:"]
-
-            transient_time = res_trans_time.get(rp_target)
-
-            transient_time_list.append(transient_time)
-
-            # print(transient_time, rp_target)
-
-        return transient_time_list
-
-    import numpy as np
-
-    class RawFileReader:
-        def get_data(self, scan: int, d_parameter: dict, scan_type: str):
-            """
-            Retrieve mass spectrometry data for a given scan.
-
-            Parameters
-            ----------
-            scan : int
-                The scan number.
-            d_parameter : dict
-                A dictionary to store additional parameters.
-            scan_type : str
-                The type of scan ("Centroid" or "Profile").
-
-            """
-            if scan_type == "Centroid":
-                centroidStream = self.iRawDataPlus.GetCentroidStream(scan, False)
-
-                noise = list(centroidStream.Noises)
-                baselines = list(centroidStream.Baselines)
-                rp = list(centroidStream.Resolutions)
-                magnitude = list(centroidStream.Intensities)
-                mz = list(centroidStream.Masses)
-
-                array_noise_std = (np.array(noise) - np.array(baselines)) / 3
-                l_signal_to_noise = np.array(magnitude) / array_noise_std
-
-                d_parameter["baseline_noise"] = np.average(array_noise_std)
-                d_parameter["baseline_noise_std"] = np.std(array_noise_std)
-
-                data_dict = {
-                    Labels.mz: mz,
-                    Labels.abundance: magnitude,
-                    Labels.rp: rp,
-                    Labels.s2n: l_signal_to_noise,
-                }
-
-            else:
-                scanStatistics = self.iRawDataPlus.GetScanStatsForScanNumber(scan)
-                profileStream = self.iRawDataPlus.GetSegmentedScanFromScanNumber(scan, scanStatistics)
-                magnitude = list(profileStream.Intensities)
-
-            
-
-                mz = list(profileStream.Positions)
-
-                data_dict = {
-                    Labels.mz: mz,
-                    Labels.abundance: magnitude,
-                }
-
-            return data_dict
-
-    def get_best_scans_idx(self, stdevs=2, method="mean", plot=False):
-            """
-            Method to determine the best scan indexes for selective co-addition.
-
-            Parameters
-            ----------
-            stdevs : int, optional
-                The number of standard deviations to use as the cutoff for filtering out datapoints. Default is 2.
-            method : str, optional
-                The method to calculate the mean or median of the TIC values. Default is "mean".
-            plot : bool, optional
-                Whether to plot the TIC with horizontal lines for the standard deviation cutoffs. Default is False.
-
-            Notes
-            -----
-            This method calculates the mean (default) or median of the TIC values and sets an upper and lower limit
-            based on a specified number of standard deviations. The scans with TIC values outside of this range are
-            considered the best scans for selective co-addition.
-
-            Empirically, using 1-2 standard deviations is enough to filter out the worst datapoints.
-
-            If `plot` is True, a matplotlib figure is returned along with the list of scan indexes.
-
-            Examples
-            --------
-            >>> reader = RawFileReader()
-            >>> scans = reader.get_best_scans_idx(stdevs=2, method="mean", plot=True)
-            """
-            tic = pd.DataFrame(self.get_tic(plot=plot))
-
-            if method == "median":
-                tic_median = tic["TIC"].median()
-            elif method == "mean":
-                tic_median = tic["TIC"].mean()
-            else:
-                print("Method " + str(method) + " undefined")
-
-            tic_std = tic["TIC"].std()
-
-            upperlimit = tic_median - (stdevs * tic_std)
-            lowerlimit = tic_median + (stdevs * tic_std)
-
-            tic_filtered = tic[(tic["TIC"] > upperlimit) & (tic["TIC"] < lowerlimit)]
-            scans = list(tic_filtered.Scans.values)
-
-            if plot:
-                import matplotlib.pyplot as plt
-
-                fig, ax = plt.subplots(figsize=(8, 4))
-                ax.plot(tic["Time"], tic["TIC"])
-                ax.axhline(y=upperlimit, c="r")
-                ax.axhline(y=lowerlimit, c="r")
-                return fig, scans
-            else:
-                return scans
-
-
-class ThermoSpectraParser(ThermoBaseClass, SpectraParserInterface):
+class ImportMassSpectraThermoMSFileReader(ThermoBaseClass, SpectraParserInterface):
     """A class for parsing Thermo RAW mass spectrometry data files and instatiating MassSpectraBase or LCMSBase objects
 
     Parameters
@@ -1057,6 +902,8 @@ class ThermoSpectraParser(ThermoBaseClass, SpectraParserInterface):
         Parses the RAW file and instantiates a MassSpectraBase object.
     * get_lcms_obj(verbose=True).
         Parses the RAW file and instantiates an LCMSBase object.
+    * get_icr_transient_times().
+        Return a list for transient time targets for all scans, or selected scans range
 
     Inherits from ThermoBaseClass and SpectraParserInterface
     """
@@ -1347,3 +1194,38 @@ class ThermoSpectraParser(ThermoBaseClass, SpectraParserInterface):
 
         return lcms_obj
         
+    def get_icr_transient_times(self):
+        """Return a list for transient time targets for all scans, or selected scans range
+
+        Notes
+        --------
+        Resolving Power and Transient time targets based on 7T FT-ICR MS system
+        """
+
+        res_trans_time = {
+            "50": 0.384,
+            "100000": 0.768,
+            "200000": 1.536,
+            "400000": 3.072,
+            "750000": 6.144,
+            "1000000": 12.288,
+        }
+
+        firstScanNumber = self.start_scan
+
+        lastScanNumber = self.end_scan
+
+        transient_time_list = []
+
+        for scan in range(firstScanNumber, lastScanNumber):
+            scan_header = self.get_scan_header(scan)
+
+            rp_target = scan_header["FT Resolution:"]
+
+            transient_time = res_trans_time.get(rp_target)
+
+            transient_time_list.append(transient_time)
+
+            # print(transient_time, rp_target)
+
+        return transient_time_list
